2f4f01a60c01134100d5dbcb3bd1ac3d7e3101ab
shoqqan <tataev.shokan@gmail.com>
2024-10-18 20:59:01 +0500

    [feat]: mantine

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,9 +2,12 @@ import Router from './router'
 import '@/shared/base.scss'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { MantineProvider } from '@mantine/core'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<Router />
+		<MantineProvider>
+			<Router />
+		</MantineProvider>
 	</StrictMode>,
 )
