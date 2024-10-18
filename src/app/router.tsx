d2635bcbafb776ab281c555ff912c925b3507b88
Gaziz <gazizbolat2@gmail.com>
2024-10-19 03:47:09 +0500

    feat: change suspense logic for router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -19,34 +19,50 @@ const router = createBrowserRouter([
 			},
 			{
 				path: 'vacancies',
-				element: <VacanciesPage />,
+				element: (
+					<Suspense fallback={<div>Loading...</div>}>
+						<VacanciesPage />
+					</Suspense>
+				),
 			},
 			{
 				path: 'vacancies/:id',
-				element: <VacancyDetailPage />,
+				element: (
+					<Suspense fallback={<div>Loading...</div>}>
+						<VacancyDetailPage />
+					</Suspense>
+				),
 			},
 			{
 				path: 'feedback',
-				element: <FeedbacksPage />,
+				element: (
+					<Suspense fallback={<div>Loading...</div>}>
+						<FeedbacksPage />
+					</Suspense>
+				),
 			},
 			{
 				path: 'tasks',
-				element: <TasksPage />,
+				element: (
+					<Suspense fallback={<div>Loading...</div>}>
+						<TasksPage />
+					</Suspense>
+				),
 			},
 			{
 				path: 'profile',
-				element: <ProfilePage />,
+				element: (
+					<Suspense fallback={<div>Loading...</div>}>
+						<ProfilePage />
+					</Suspense>
+				),
 			},
 		],
 	},
 ])
 
 function Router() {
-	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<RouterProvider router={router} />
-		</Suspense>
-	)
+	return <RouterProvider router={router} />
 }
 
 export default Router
