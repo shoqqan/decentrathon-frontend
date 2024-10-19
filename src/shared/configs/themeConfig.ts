54a97d169d8130f61d1d937968b1eff37251e644
shoqqan <tataev.shokan@gmail.com>
2024-10-19 20:29:18 +0500

    [feat] styled profile

--- src/shared/configs/themeConfig.ts
+++ src/shared/configs/themeConfig.ts
@@ -3,6 +3,7 @@ import { SelectExtend } from '../ui/Select'
 import { TextareaExtend } from '../ui/Textarea/Textarea'
 import { TextInputExtend } from '../ui/TextInput'
 import { createTheme } from '@mantine/core'
+import { MonthPickerInputExtend } from '../ui/MonthPickerInput'
 
 export const themeConfig = createTheme({
 	primaryColor: 'blue',
@@ -11,5 +12,6 @@ export const themeConfig = createTheme({
 		Drawer: DrawerExtend,
 		Textarea: TextareaExtend,
 		Select: SelectExtend,
+		MonthPickerInput: MonthPickerInputExtend
 	},
 })
--- src/shared/ui/MonthPickerInput/MonthPickerInput.tsx
+++ src/shared/ui/MonthPickerInput/MonthPickerInput.tsx
@@ -1,7 +1,11 @@
 import { MonthPickerInput } from '@mantine/dates'
+import styles from "./MonthPickerInput.module.scss"
 
 const MonthPickerInputExtend = MonthPickerInput.extend({
-
+	classNames:{
+		wrapper: styles.month_picker,
+		input: styles.month_picker_input
+	}
 })
 
 export {MonthPickerInputExtend, MonthPickerInput}
