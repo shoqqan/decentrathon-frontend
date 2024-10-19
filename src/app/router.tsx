5393affb903a147ed5291d8bd4de01d5ec730637
Gaziz <gazizbolat2@gmail.com>
2024-10-19 17:22:38 +0500

    refactor: remove lazy-load for routes

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,13 +1,17 @@
 import VacanciesPage from '../pages/VacanciesPage/VacanciesPage'
+import FeedbacksPage from '@/pages/FeedbacksPage/FeedbacksPage'
+import ProfilePage from '@/pages/ProfilePage/ProfilePage'
+import TasksPage from '@/pages/TasksPage/TasksPage'
+import VacancyDetailPage from '@/pages/VacancyDetailPage/VacancyDetailPage'
 import { MainWrapper } from '@/widgets/MainWrapper'
-import { lazy, Suspense } from 'react'
+import { Suspense } from 'react'
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 
 // const VacanciesPage = lazy(() => import('../pages/VacanciesPage/VacanciesPage'))
-const FeedbacksPage = lazy(() => import('../pages/FeedbacksPage/FeedbacksPage'))
-const TasksPage = lazy(() => import('../pages/TasksPage/TasksPage'))
-const ProfilePage = lazy(() => import('../pages/ProfilePage/ProfilePage'))
-const VacancyDetailPage = lazy(() => import('../pages/VacancyDetailPage/VacancyDetailPage'))
+// const FeedbacksPage = lazy(() => import('../pages/FeedbacksPage/FeedbacksPage'))
+// const TasksPage = lazy(() => import('../pages/TasksPage/TasksPage'))
+// const ProfilePage = lazy(() => import('../pages/ProfilePage/ProfilePage'))
+// const VacancyDetailPage = lazy(() => import('../pages/VacancyDetailPage/VacancyDetailPage'))
 
 const router = createBrowserRouter([
 	{
